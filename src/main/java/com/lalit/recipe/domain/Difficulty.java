ae5440035ea85c0fb69566a97c05b5a74fca8e21
lalit <lalit@192.168.1.10>
2017-08-06 18:11:21 +0100

    Added Enum for diffiulty

---
+++ src/main/java/com/lalit/recipe/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.lalit.recipe.domain;
+
+public enum Difficulty {
+	
+	EASY, MODERATE, KIND_OF_HARD, HARD 
+
+}
--- src/main/java/com/lalit/recipe/domain/Recipe.java
+++ src/main/java/com/lalit/recipe/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,8 +27,9 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	//TODO add
-	//private Difficulty difficulty;
+	
+	@Enumerated(EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
@@ -124,6 +127,15 @@ public class Recipe {
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	
 	
 	
 }
