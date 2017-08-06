2eea360cb6545553c65582c30de6130a94dfde11
Lalit Suryawanshi <lalit@Lalits-MacBook-Pro.local>
2017-08-06 13:22:41 +0100

    First Commit

---
+++ src/main/java/com/lalit/recipe/SpringBootReceipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.lalit.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReceipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReceipeAppApplication.class, args);
+	}
+}
