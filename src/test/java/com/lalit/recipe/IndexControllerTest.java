1a228917e72c391918ad0a44e810f11c2c3bf96e
lalit <lalit@192.168.1.7>
2017-08-11 23:45:15 +0100

    Added MockMVC Tests Support

--- src/test/java/com/lalit/recipe/IndexControllerTest.java
+++ src/test/java/com/lalit/recipe/IndexControllerTest.java
@@ -4,9 +4,14 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.net.ssl.SSLEngineResult.Status;
 
 import static org.mockito.ArgumentMatchers.eq;
 
@@ -16,6 +21,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.ui.Model;
 
 import com.lalit.recipe.controllers.IndexController;
@@ -39,6 +47,14 @@ public class IndexControllerTest {
 		controller = new IndexController(recipeService);
 	}
 
+	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("index"));
+	}
+	
 	@Test
 	public void testGetIndexPage() throws Exception {
 		
