5aaaef537502b036c45aa8fdbf6f95308157ab10
lalit <lalit@192.168.0.108>
2017-08-12 22:18:53 +0100

    Added Command Objects

---
+++ src/main/java/com/lalit/recipe/commands/IngredientCommand.java
@@ -0,0 +1,19 @@
+package com.lalit.recipe.commands;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IngredientCommand {
+	
+	private Long id;
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasureCommand uom;
+
+}
---
+++ src/main/java/com/lalit/recipe/commands/NotesCommand.java
@@ -0,0 +1,17 @@
+package com.lalit.recipe.commands;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+	
+	private Long id;
+    private String description;
+
+
+}
